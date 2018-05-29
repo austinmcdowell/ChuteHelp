a8e9192224a248d04b66eb8da260847ba7350987
TJ Barber <me@tjbarber.org>
2018-05-29 15:41:17 -0600

    Added google analytics and adroll

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -176,6 +176,36 @@
             </div>
             
         </div>
-        
+        <script type="text/javascript">
+            adroll_adv_id = "NRRMMNGCSBCYVPPUA5AWLU";
+            adroll_pix_id = "W5RZYIEWAVASNDVETP7O2J";
+            /* OPTIONAL: provide email to improve user identification */
+            /* adroll_email = "username@example.com"; */
+            (function () {
+                var _onload = function(){
+                    if (document.readyState && !/loaded|complete/.test(document.readyState)){setTimeout(_onload, 10);return}
+                    if (!window.__adroll_loaded){__adroll_loaded=true;setTimeout(_onload, 50);return}
+                    var scr = document.createElement("script");
+                    var host = (("https:" == document.location.protocol) ? "https://s.adroll.com" : "http://a.adroll.com");
+                    scr.setAttribute('async', 'true');
+                    scr.type = "text/javascript";
+                    scr.src = host + "/j/roundtrip.js";
+                    ((document.getElementsByTagName('head') || [null])[0] ||
+                        document.getElementsByTagName('script')[0].parentNode).appendChild(scr);
+                };
+                if (window.addEventListener) {window.addEventListener('load', _onload, false);}
+                else {window.attachEvent('onload', _onload)}
+            }());
+        </script>
+
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-41915592-2"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-41915592-2');
+        </script>
     </body>
 </html>
