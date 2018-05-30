a4c32679221611cbb5ecf62bd3c833b061d66f21
TJ Barber <me@tjbarber.org>
2018-05-30 10:14:01 -0600

    Contact form sends something, need to hook up mailgun

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -27,4 +27,31 @@ class SiteController extends Controller
             'page' => $page
         ]);
     }
+
+    public function send_email(Request $request)
+    {
+        $firstname = $request->input('firstname');
+        $lastname  = $request->input('lastname');
+        $email     = $request->input('email');
+        $phone     = $request->input('phone');
+        $message   = $request->input('message');
+        $zip_code  = $request->input('zipcode');
+        $become_retailer   = ($request->input('becomeRetailer') ? true : false);
+        $roping_equipment  = ($request->input('ropingEquipment') ? true : false);
+        $cattle_equipment  = ($request->input('cattleEquipment') ? true : false);
+        $easy_now_products = ($request->input('easyNowProducts') ? true : false);
+
+        return json_encode([
+            'firstname' => $firstname,
+            'lastname' => $lastname,
+            'email' => $email,
+            'phone' => $phone,
+            'message' => $message,
+            'zipcode' => $zip_code,
+            'become_retailer' => $become_retailer,
+            'roping_equipment' => $roping_equipment,
+            'cattle_equipment' => $cattle_equipment,
+            'easy_now_products' => $easy_now_products
+        ]);
+    }
 }
